f6f86e3c26873e1fc23c8989f5a0f938dbdd541c
Francesco Persico <francescopersico@gmail.com>
2022-02-22 16:19:27 +0100

    Use BASE_PATH

--- index.js
+++ index.js
@@ -4,21 +4,27 @@ const morgan = require("morgan");
 const { makeIndexRouter } = require("./routes");
 
 const SERVER_PORT = process.env.PORT;
+const CGAAP_BASE_PATH = process.env.CGAAP_BASE_PATH || "/";
 
 const app = express();
-
-// Middlewares
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
-
-// Serve static files
-app.use(express.static(path.join(__dirname, "public")));
 
 // Configuring express template engine
 app.set("view engine", "ejs");
 
+// Create main router
+const router = express.Router();
+
+// Middlewares
+router.use(express.urlencoded({ extended: true }));
+router.use(morgan("dev"));
+
+// Serve static files
+router.use(express.static(path.join(__dirname, "public")));
+
 // Routes
-app.use("/", makeIndexRouter());
+router.use("/", makeIndexRouter());
+
+app.use(CGAAP_BASE_PATH, router);
 
 // Start server
 app.listen(SERVER_PORT, () => {
